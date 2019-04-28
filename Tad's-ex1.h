d5eb60f1865b0349e36804c9976c4a754dcd47ca
Malva03 <49702172+Malva03@users.noreply.github.com>
2019-04-28 20:21:58 -0300

    Delete Tad's-ex1.h

--- Tad's-ex1.h
+++
@@ -1,8 +0,0 @@
-#ifndef TAD'S-EX1_H_INCLUDED
-#define TAD'S-EX1_H_INCLUDED
-int Menu();
-int Checar_data();
-void SettarLista(Lista);
-void Inserir_na_lista(Aux, Lista);
-
-#endif // TAD'S-EX1_H_INCLUDED
